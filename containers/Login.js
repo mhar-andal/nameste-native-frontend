a67d3f2cd6d1559ac8f03bbc1902b155dd977609
Mhar Andal <mhar.andal@gamut.com>
2017-12-09 17:27:36 -0600

    commit before huge refactor

--- containers/Login.js
+++ containers/Login.js
@@ -8,6 +8,7 @@ import {
   View
 } from 'react-native';
 import axios from 'axios';
+import { isEmpty } from 'lodash';
 
 export default class Login extends Component {
   constructor(props) {
@@ -17,42 +18,101 @@ export default class Login extends Component {
 
     this.state = {
       email: '',
+      username: '',
       password: '',
+      firstName: '',
+      lastName: '',
       emailPlaceholder: 'example@gmail.com',
+      usernamePlaceholder: 'johndoe10',
       passwordPlaceholder: '············',
+      firstNamePlaceholder: 'Joe',
+      lastNamePlaceholder: 'Doe',
       width: width - 40,
       height,
       isAuthenticated: false,
-      error: []
+      errors: []
     };
     this.handleLogin = this.handleLogin.bind(this);
   }
 
   handleLogin() {
-    axios.post('http://localhost:3000/v1/users')
+    const {
+      email,
+      username,
+      password,
+      firstName,
+      lastName,
+    } = this.state;
+
+    axios.post('http://localhost:3000/v1/users', {
+        email: email,
+        username: username,
+        password: password,
+        first_name: firstName,
+        last_name: lastName,
+    })
     .then((response) => {
-      console.log('response', response);
+      this.setState({
+        sessionId: response.data.id,
+        isAuthenticated: true,
+      })
+    })
+    .catch((error) => {
+      this.setState({ errors: error.response.data.errors });
     });
   }
 
+  renderErrors(errors) {
+    return (
+      <Text>Username has already been created</Text>
+    )
+  }
+
   renderLoginForm() {
-    const { width } = this.state;
+    const { errors, width } = this.state;
     return (
       <View style={styles.container}>
+        {isEmpty(errors) && this.renderErrors(errors)}
         <Text>Email</Text>
         <TextInput
           style={{ height: 30, width: width, borderColor: 'black', borderWidth: 1 }}
-          onChange={(email) => this.setState({ email })}
+          onChangeText={(email) => this.setState({ email })}
           value={this.state.email}
+          autoCorrect={false}
           placeholder={this.state.emailPlaceholder}
+        />
+        <Text>Username</Text>
+        <TextInput
+          style={{ height: 30, width: width, borderColor: 'black', borderWidth: 1 }}
+          onChangeText={(username) => this.setState({ username })}
+          value={this.state.username}
+          autoCorrect={false}
+          placeholder={this.state.usernamePlaceholder}
         />
         <Text>Password</Text>
         <TextInput
           style={{ height: 30, width: width, borderColor: 'black', borderWidth: 1 }}
-          onChange={(password) => this.setState({ password })}
+          onChangeText={(password) => this.setState({ password })}
           value={this.state.password}
           secureTextEntry={true}
+          autoCorrect={false}
           placeholder={this.state.passwordPlaceholder}
+        />
+        <Text>First Name</Text>
+        <TextInput
+          style={{ height: 30, width: width, borderColor: 'black', borderWidth: 1 }}
+          onChangeText={(firstName) => this.setState({ firstName })}
+          value={this.state.firstName}
+          autoCorrect={false}
+          placeholder={this.state.firstNamePlaceholder}
+        />
+        <Text>Last Name</Text>
+        <TextInput
+          style={{ height: 30, width: width, borderColor: 'black', borderWidth: 1 }}
+          onChangeText={(lastName) => this.setState({ lastName })}
+          value={this.state.lastName}
+          autoCorrect={false}
+          placeholder={this.state.lastNamePlaceholder}
         />
         <Button
           onPress={this.handleLogin}
