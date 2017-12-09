010505f8e3c7facc14da3e5786eaac33016d2575
Mhar Andal <mhar.andal@gamut.com>
2017-12-09 12:55:38 -0600

    setup simple api call to rails backend

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
+const DEFAULT_NAME = 'Somebody'
 
 export default class App extends React.Component {
+  state = {};
+
+  componentWillMount() {
+    axios.get('http://localhost:3000/test/test')
+    .then((response) => {
+      console.log('response', response);
+      const { name } = response.data
+      this.setState({
+        name
+      })
+    });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.state.name || DEFAULT_NAME}</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
